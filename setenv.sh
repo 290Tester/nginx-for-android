fa5bcf1e69e50b9cd0aab87d92d9ab6a3d844ec9
290Tester <290Tester@github>
2025-12-13 23:27:21 +0800

    v1.29.4

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+wget -q https://dl.google.com/android/repository/android-ndk-r27b-linux.zip
+unzip -q android-ndk-r27b-linux.zip
 export NDK="$PWD/android-ndk-r27b"
 export CC="$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang"
 export CXX="$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang++"
 export PATH="$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
 export CFLAGS="-fPIE -fPIC"
+./Configuration.nginx
