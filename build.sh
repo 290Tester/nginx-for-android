6d9fbff44039515dd43b1ce03d7c7edba14fe1ae
290Tester <290Tester@github>
2025-12-20 02:11:00 +0800

    1

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir -p /data/web/Nginx
+cd /data/web/Nginx
 echo ">>> Downloading NDK r27b..."
 wget -q https://dl.google.com/android/repository/android-ndk-r27b-linux.zip
 unzip -q android-ndk-r27b-linux.zip
@@ -13,29 +15,34 @@ export LD=$TOOLCHAIN/bin/ld
 export AR=$TOOLCHAIN/bin/llvm-ar
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export CFLAGS="-fPIE -fPIC"
-echo ">>> Extracting depend..."
-tar -xf depend.tar.gz
+echo ">>> Extracting..."
+wget "https://github.com/nginx/nginx/releases/download/release-1.29.4/nginx-1.29.4.tar.gz"
+tar -xf "nginx-1.29.4.tar.gz"
+wget "https://github.com/290Tester/nginx-for-android/releases/download/v1.29.4/Build-dep.tar.gz"
+tar -xf "Build-dep.tar.gz"
 echo ">>> Configuring nginx..."
+cd "nginx-1.29.4"
 ls -l $CC
 $CC --version
 auto/configure \
 --prefix=/data/web/Nginx \
---conf-path=/data/web/Nginx/etc \
---error-log-path=/data/web/Nginx/var \
---http-log-path=/data/web/Nginx/var \
---pid-path=/data/web/Nginx/var \
---lock-path=/data/web/Nginx/var \
+--conf-path=/data/web/Nginx/etc/nginx.conf \
+--error-log-path=/data/web/Nginx/var/error.log \
+--http-log-path=/data/web/Nginx/var/log.log \
+--pid-path=/data/web/Nginx/var/nginx.pid \
+--lock-path=/data/web/Nginx/var/nginx.lock \
 --http-client-body-temp-path=/data/web/Nginx/var/client_body_temp \
 --http-proxy-temp-path=/data/web/Nginx/var/proxy_temp \
 --http-fastcgi-temp-path=/data/web/Nginx/var/fastcgi_temp \
 --http-uwsgi-temp-path=/data/web/Nginx/var/uwsgi_temp \
 --http-scgi-temp-path=/data/web/Nginx/var/scgi_temp \
---with-pcre=/data/web/Nginx \
---with-openssl=/data/web/Nginx \
+--with-pcre=/data/web/Core \
+--with-openssl=/data/web/Core \
+--with-zlib=/data/web/Core \
 --with-pcre-opt=--host=aarch64-linux-android \
---with-cc=aarch64-linux-android35-clang \
---with-cc-opt='-I/data/web/Nginx/include -D__USE_GNU' \
---with-ld-opt='-L/data/web/Nginx/lib -lm -lssl -lcrypto -Wl,-rpath,/data/web/Nginx/lib:/data/web/Nginx/lib64' \
+--with-cc=$CC \
+--with-cc-opt='-I/data/web/Core/include -D__USE_GNU -Os -flto ' \
+--with-ld-opt='-L/data/web/Core/lib -lz /data/web/Core/.libs/libpcre.a /data/web/Core/.openssl/lib/libssl.a /data/web/Core/.openssl/lib/libcrypto.a -Wl,-rpath,/data/web/Core/lib:/data/web/Core/lib64 -lz /data/web/Core/.libs/libpcre.a /data/web/Core/.openssl/lib/libssl.a /data/web/Core/.openssl/lib/libcrypto.a -Wl,--gc-sections -flto' \
 --with-http_ssl_module \
 --with-stream_ssl_module \
 --with-http_v2_module \
